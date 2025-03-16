8a50b96c4bfe9f7cf59d57a65c9730e4d62661c9
FabulousBernt <johnny.vigersten@icloud.com>
2025-03-16 08:06:07 +0100

    refactored while loop

--- main.py
+++ main.py
@@ -8,15 +8,13 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    number = 1
-    while number != 0:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill(color="#000000")
+        screen.fill("black")
         pygame.display.flip()
-
 
 if __name__ == "__main__":
     main()
