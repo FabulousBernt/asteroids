27952d6f8cdca75cace90d8c599672c89c5424c3
FabulousBernt <johnny.vigersten@icloud.com>
2025-03-18 07:56:20 +0100

    added player and circle shape files

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
+from player import Player
 from constants import *
+
 
 def main():
     pygame.init()
@@ -10,14 +12,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    game_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        game_player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
